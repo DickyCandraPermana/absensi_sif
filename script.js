a4a28b0627eacc799a12557292c7846da79f346f
DickyCandraPermana <dickycandrapermana@student.uns.ac.id>
2025-08-29 02:09:52 +0700

    hoping this is my last change

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const data = [
   {
     name: "Vokasi, Prompting, dan Vibe Coding: Siap menghadapi AI-driven Industry?",
     speaker: "Cycas Rifky",
-    startTime: "00:20",
+    startTime: "08:20",
     endTime: "15:55",
     day: 1,
   },
